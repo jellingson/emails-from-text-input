efde9e9a5da306e0cc7c4d825a1aebe7052e6efa
jellingson <rand_AL_th0r>
2019-03-12 11:28:39 -0600

    Simple demo for getting emails from text on a website. Still needs more testing and a UI mockup.

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
+from flask import Flask, render_template, request, jsonify
+
 import re
 
-regex = re.compile('[^\s]+@[^\s]+\.[^,;:\s]+')
+app = Flask(__name__)
 
-raw_text = "j@ed.com asdfds google@sdf.com sfd7t@sf.fg afgljkh@sfsad @asfs.com, ashdfg"
-emails = regex.findall(raw_text)
 
-for email in emails:
-    print(email)
+@app.route("/projects/get-email-application")
+def get_emails_application():
+    return render_template("email-application.html")
+
+
+@app.route("/projects/email-results")
+def show_results():
+    text = request.args.get('text', "", type=str)
+    emails = get_emails(text)
+    print(text)
+
+    return jsonify(result=emails)
+
+
+def get_emails(text):
+    regex = re.compile('[^\s]+@[^\s]+\.[^,;:\s]+')
+
+    emails = regex.findall(text)
+    emailList = []
+
+    for email in emails:
+        emailList.append(email)
+        print(email)
+
+    return emailList
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
